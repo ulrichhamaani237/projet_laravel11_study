2e64ad912b724537650304bf5b6e69cf110d594e
ulrich hamaani <119246440+ulrichhamaani237@users.noreply.github.com>
2024-09-20 14:02:12 +0100

    v7_logout_function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(Request $request){
 
        return View('admin.index');
-    }    
+    } 
+    
+    public function AdminLogout(Request $request){
+     
+    Auth::guard('web')->logout();
+     $request->session()->invalidate();
+     $request->session()->regenerateToken();
+     return redirect('/login');
+       
+    }
+    
 }
+
